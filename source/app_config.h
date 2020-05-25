99373801fb58a281bd60750355a0be9efe8e149b
Marek Vitula <marek.vitula@nxp.com>
2020-05-25 13:59:22 +0200

    Updated app configuration

--- source/app_config.h
+++ source/app_config.h
@@ -2,7 +2,7 @@
  * app_config.h
  *
  *  Created on: 7 May 2019
- *      Author: nxf05489
+ *      Author: Marek Vitula
  */
 
 #ifndef APP_CONFIG_H_
@@ -19,7 +19,25 @@
 /* Runtime of the motor while locking/unlocking in milliseconds. */
 #define SF_MOTOR_RUNTIME_MS         2500
 
-/* Duty cycle of H-Bridge PWM (0-100). */
-#define SF_MOTOR_DUTY               90
+/* Sigfox message size in bytes
+ * without added padding */
+#define SF_MSG_SIZE 5
+
+/* User database settings */
+/* Number of users in DB */
+#define USERS 32
+
+/* Size of the Mifare authentication Key, 6 bytes for Mifare Classic */
+#define MIFARE_SIZE 6
+
+/* Size of the UID (NUID) NFC tag identification number, could be 4 or 7 bytes */
+#define UID_SIZE 4
+
+/* Authentication key (password) size */
+#define KEY_SIZE 16
+
+/* Block number where are the keys stored 
+ * Sector number is block/4 */
+#define BLK_NB_MFC 4
 
 #endif /* APP_CONFIG_H_ */
